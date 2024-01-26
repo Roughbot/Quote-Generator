4204021bf97706b03b7dde3cda621f2fe6ce1994
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-01-27 02:23:30 +0530

    cache fit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponents: true,
+    concurrentFeatures: true,
+  },
+  future: {
+    webpack5: true,
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.cache = {
+      type: "filesystem",
+      cacheDirectory: ".next/cache",
+    };
+
+    return config;
+  },
+};
 
 export default nextConfig;
