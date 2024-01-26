2d24be269bdaf61f675a83ab70aef58a8c1767d9
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-01-27 02:37:33 +0530

    bug fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
-import { resolve } from "path";
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.cache = {
-      type: "filesystem",
-      cacheDirectory: resolve(".next/cache"),
-    };
-
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
