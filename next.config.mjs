782a21997d1f5f343ba422996321b5a8ac4afc7a
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-01-27 02:28:00 +0530

    cache

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponents: true,
-    concurrentFeatures: true,
-  },
-  future: {
-    webpack5: true,
-  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.cache = {
       type: "filesystem",
